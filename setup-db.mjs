b8b2eeedda1050c873535276ee153fa12c80c72c
codeasaneng <bcaeirourbano@gmail.com>
2023-10-12 13:06:54 +0100

    Update setup-deb to run migrations file

--- setup-db.mjs
+++ setup-db.mjs
@@ -1,5 +1,8 @@
 import { config } from 'dotenv';
 import pg from 'pg';
+import { exec } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const { Pool } = pg;
 
@@ -25,16 +28,38 @@ pool.query(`SELECT 1 FROM pg_database WHERE datname='${database}'`, (err, res) =
     }
     
     if (res.rowCount === 0) {
-        pool.query(`CREATE DATABASE ${database}`, (creationErr, creationRes) => {
+        pool.query(`CREATE DATABASE ${database}`, (creationErr) => {
             if (creationErr) {
                 console.error('Error creating database:', creationErr);
             } else {
                 console.log('Database created successfully');
+                runMigrations();  // Run migrations after creating the database
             }
             pool.end();
         });
     } else {
         console.log('Database already exists.');
+        runMigrations();  // Run migrations if the database already exists
         pool.end();
     }
 });
+
+function runMigrations() {
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const migrateFilePath = path.join(__dirname, 'src', 'migrations', 'migrate.sql');
+    const command = `psql -U ${user} -d postgres -a -f ${migrateFilePath}`;
+
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Migration error: ${error.message}`);
+            return;
+        }
+
+        if (stderr) {
+            console.error(`Migration stderr: ${stderr}`);
+            return;
+        }
+
+        console.log(`Migration stdout: ${stdout}`);
+    });
+}
